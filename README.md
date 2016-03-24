1e0255349079cc412ba4b3d44e65ded9c2d44657
doctorkey <wangguohua_key@163.com>
2016-03-25 00:13:45 +0800

    自己做飞控
    比赛细则，要求自己做飞控，项目重写

--- README.md
+++
@@ -1,7 +0,0 @@
-# aircraft_code
-这是2016年光电大赛的代码
-
-stm32f407
-
-A9---TX
-A10--RX
--- USER/main.c
+++ USER/main.c
@@ -1,33 +1,66 @@
-
-
-
 #include "stm32f4xx.h"
-#include "LED.H"
-#include "usart1.h"
+#include "usart.h"
+#include "delay.h"
 
-void delay(unsigned int count);
+//ALIENTEK 探索者STM32F407开发板 实验0
+//STM32F4工程模板-库函数版本
+//技术支持：www.openedv.com
+//淘宝店铺：http://eboard.taobao.com
+//广州市星翼电子科技有限公司  
+//作者：正点原子 @ALIENTEK
 
 int main(void)
 {
-	u8 send_data='1';
-	LED_GPIO_Config();
-	USART1_Config();
+	u32 t=0;
+	uart_init(115200);
+	delay_init(84);
+	
+  while(1){
+    printf("t:%d\r\n",t);
+		delay_ms(500);
+		t++;
+	}
+}
 
-while(1)
+/*
+手册中讲解到步骤15的时候的main.c源码如下：
+#include "stm32f4xx.h"
+
+//ALIENTEK 探索者STM32F407开发板 实验0
+//STM32F4工程模板-库函数版本
+//技术支持：www.openedv.com
+//淘宝店铺：http://eboard.taobao.com
+//广州市星翼电子科技有限公司  
+//作者：正点原子 @ALIENTEK
+  
+void Delay(__IO uint32_t nCount);
+
+void Delay(__IO uint32_t nCount)
 {
-//	delay(444000);
-//	send_data=USART_ReceiveData(USART1);
-//	USART_SendData(USART1,send_data);
-//	while(USART_GetFlagStatus(USART1,USART_FLAG_TXE)==RESET);
-}
+  while(nCount--){}
 }
 
-
-
-void delay(unsigned int count)
+int main(void)
 {
-int i;
-for(i=0;i<count;i++);
+
+  GPIO_InitTypeDef  GPIO_InitStructure;
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+  while(1){
+		GPIO_SetBits(GPIOF,GPIO_Pin_9|GPIO_Pin_10);
+		Delay(0x7FFFFF);
+		GPIO_ResetBits(GPIOF,GPIO_Pin_9|GPIO_Pin_10);
+		Delay(0x7FFFFF);
+	
+	}
 }
+*/
 
 
